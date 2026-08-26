6e654f4e914df97eb18c94d936ccebe5690d6899
Vanessa Morales <auxiliadoravanessam@gmail.com>
2026-08-26 10:45:06 -0600

    En el layout principal de app Se agregó la ruta de entrevista y se quitó la de bienvenida.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,71 +12,58 @@ import { AuthProvider, useAuth } from "@/services/authProvider";
 
 SplashScreen.preventAutoHideAsync();
 
-// NAVEGACIÓN PRINCIPAL
 function RootNavigation() {
   const { loading, session } = useAuth();
   const router = useRouter();
 
-  // REDIRECCIÓN SEGÚN SESIÓN
   useEffect(() => {
-    // Esperar a que AuthProvider termine authLoading y profileLoading
     if (loading) return;
 
-    // USUARIO AUTENTICADO
     if (session) {
-      router.replace("/(tabs)/home");
+      router.replace("/(entrevista)/bienvenida");
       return;
     }
 
-    // USUARIO NO AUTENTICADO
     router.replace("/(auth)/welcome");
   }, [loading, session, router]);
 
-  // CARGANDO AUTENTICACIÓN
   if (loading) {
     return <AnimatedLogo />;
   }
 
-  // NAVEGADOR
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(bienvenida)" />
+      <Stack.Screen name="(entrevista)" />
     </Stack>
   );
 }
 
-// ROOT LAYOUT
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  // FUENTES
   const [loaded, error] = useFonts({
     "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
     "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
     "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
   });
 
-  // ERROR DE FUENTES
   useEffect(() => {
     if (error) throw error;
   }, [error]);
 
-  // OCULTAR SPLASH NATIVO
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
 
-  // Mientras cargan las fuentes mantenemos el SplashScreen nativo
   if (!loaded) {
     return null;
   }
 
-  // APP
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AuthProvider>
