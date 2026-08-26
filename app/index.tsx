9b8d48b3df26a4d94c27a3c53601f013c08876c5
Vanessa Morales <auxiliadoravanessam@gmail.com>
2026-08-26 13:18:24 -0600

    Se realizó la importación del componente AnimatedLogo

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Redirect } from "expo-router";
 //import AnimatedLogo from '@/components/ui/AnimatedLogo';
 import React from 'react';
+import AnimatedLogo from "@/components/ui/AnimatedLogo";
 
 // Pantalla inicial de Kiri.
 // app/_layout.tsx controla el tiempo minimo del splash
