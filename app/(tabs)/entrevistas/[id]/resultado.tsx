464cd5bfdcbcd880ee9ceec610ab6b425527e62e
Vanessa Morales <auxiliadoravanessam@gmail.com>
2026-08-27 15:47:23 -0600

    Manda a llamar el componente ResultadoEntrevista pasandole el modo historial

---
+++ app/(tabs)/entrevistas/[id]/resultado.tsx
@@ -0,0 +1,7 @@
+import ResultadoEntrevista from "@/components/entrevista/ResultadoEntrevista";
+import React from "react";
+export default function ResultadoHistorialScreen() {
+  return (
+    <ResultadoEntrevista modo="historial" />
+  );
+}
