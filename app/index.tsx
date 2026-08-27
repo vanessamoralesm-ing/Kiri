43c39375d453e2b8cf8b96d64712cc735c789b1b
maricarmen-ig <igmaricarmenorozco7@gmail.com>
2026-08-27 00:28:35 -0600

    Corrigiendo errores en el splash e inicio de la app

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,8 @@
-import { Redirect } from "expo-router";
-//import AnimatedLogo from '@/components/ui/AnimatedLogo';
+import AnimatedLogo from '@/components/ui/AnimatedLogo';
 import React from 'react';
 
 // Pantalla inicial de Kiri.
 // app/_layout.tsx controla el tiempo minimo del splash
-// y, cuando AuthProvider termina de comprobar la sesion,
-// redirige a welcome o home segun corresponda.
 export default function Index() {
   return <AnimatedLogo />;
-
-  //return <Redirect href="/cuestionarios" />; //Redireccionamiento temporal a la pantalla de cuestionarios para pruebas.
-
 }
