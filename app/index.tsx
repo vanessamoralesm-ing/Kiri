a08324c28385446e305f181572ee81ac476a6329
Vanessa Morales <auxiliadoravanessam@gmail.com>
2026-08-27 15:13:53 -0600

    Solo se agregó el import de la animación del splash

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 //import AnimatedLogo from '@/components/ui/AnimatedLogo';
 import React from 'react';
-
+import AnimatedLogo from "@/components/ui/AnimatedLogo";
 // Pantalla inicial de Kiri.
 // app/_layout.tsx controla el tiempo minimo del splash
 // y, cuando AuthProvider termina de comprobar la sesion,
